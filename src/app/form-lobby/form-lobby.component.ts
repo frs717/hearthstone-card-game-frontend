b47f50135c9018c792d918af427ce4bb184c0f45
naslobakina@gmail.com <naslobakina@gmail.com>
2022-07-08 14:39:15 +0300

    implement connection part of lobby

--- src/app/form-lobby/form-lobby.component.ts
+++ src/app/form-lobby/form-lobby.component.ts
@@ -1,5 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-form-lobby',
@@ -8,13 +10,25 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 })
 
 export class FormLobbyComponent implements OnInit {
-  @Output() isClickBtn= new EventEmitter<void>();
+  @Output() isClickBtn = new EventEmitter<void>();
   tittle = 'Создать игру';
   flag = true;
+  stompClient: any;
+  disabled = true;
+  greetings: string[] = [];
+  name!: string;
 
   constructor() {
+
   }
 
+  setConnected(connected: boolean) {
+    this.disabled = !connected;
+
+    if (connected) {
+      this.greetings = [];
+    }
+  }
   form!: FormGroup;
   btnInEnabled = false;
   btnCreateEnabled = false;
@@ -29,10 +43,35 @@ export class FormLobbyComponent implements OnInit {
       this.btnCreateEnabled = x.name;
       this.btnInEnabled = x.name && x.idGame;
     });
+
   }
 
   hiddenForm() {
     this.isClickBtn.emit();
+  }
+  connect() {
+    const socket = new SockJS('http://localhost:8080/ws');
+    this.stompClient = Stomp.over(socket);
+    const _this = this;
+    this.stompClient.connect({}, function (frame:object) {
+      _this.setConnected(true);
+      console.log('Connected: ' + frame);
+      _this.stompClient.subscribe('/topic/public/test', function (hello: any) {
+        _this.showGreeting(JSON.parse(hello.body).id);
+      });
+    });
+  }
+
+  showGreeting(message: string) {
+    this.greetings.push(message);
+  }
+
+  sendName() {
+    this.stompClient.send(
+      '/app/test.test',
+      {},
+      JSON.stringify({'name':"nastyaaaaaaa"})
+    );
   }
 }
 
