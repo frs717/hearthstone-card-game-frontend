737d571a8f57ea6fca3452e6e82512455f2a48bf
naslobakina@gmail.com <naslobakina@gmail.com>
2022-07-15 14:05:58 +0300

    fix output cards

--- src/app/card-inventory/card-inventory.component.ts
+++ src/app/card-inventory/card-inventory.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card-inventory',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-inventory.component.scss']
 })
 export class CardInventoryComponent implements OnInit {
-
+  @Input()
+  cardI:any;
   constructor() { }
 
   ngOnInit(): void {
