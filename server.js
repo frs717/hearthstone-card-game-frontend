e8c0642410c567b6ee98f466c18724fb71205360
Ivan Semikov <semikov.exf@gmail.com>
2022-07-24 20:36:48 +0300

    updates to deploy to heroku x10

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const express = require('express');
 const app = express();
 
 app.use(requireHTTPS);
-app.use(express.static('./dist/hearthstone-card-game-angular'));
+app.use(express.static('./dist/ang-card-game'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/hearthstone-card-game-angular/'}),
+  res.sendFile('index.html', {root: 'dist/ang-card-game/'}),
 );
 
 app.listen(process.env.PORT || 8080);
