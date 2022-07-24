0b2380ffde6f03ba90e52c35dc97081c1369da4e
Ivan Semikov <semikov.exf@gmail.com>
2022-07-24 20:27:19 +0300

    updates to deploy to heroku x8

--- server.js
+++ server.js
@@ -1,39 +1,19 @@
-'use strict';
-
-/* Подключаем Zone.js для сервера */
-require('zone.js/dist/zone-node');
-
-const express = require('express');
-const ngUniversal = require('@nguniversal/express-engine');
-
-const appServer = require('./dist-server/main.bundle');
-
-/* Рендеринг на стороне сервера */
-function angularRouter(req, res) {
-
-  res.render('index', { req, res });
-
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
 }
 
+const express = require('express');
 const app = express();
 
-/* Направляем роут в корень нашего приложения*/
-app.get('/', angularRouter);
+app.use(requireHTTPS);
+app.use(express.static('./dist/angular-heroku'));
 
-/* Отдаем статические файлы генерируемые CLI  (index.html, CSS? JS, assets...) */
-app.use(express.static(`${__dirname}/dist`));
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+);
 
-/*Конфигурируем движок Angular Express */
-app.engine('html', ngUniversal.ngExpressEngine({
-  bootstrap: appServer.AppServerModuleNgFactory
-}));
-app.set('view engine', 'html');
-app.set('views', 'dist');
-
-/* Direct all routes to index.html, where Angular will take care of routing */
-app.get('*', angularRouter);
-app.post('*', angularRouter);
-
-app.listen(3000, () => {
-  console.log(`Listening on http://localhost:3000`);
-});
+app.listen(process.env.PORT || 8080);
