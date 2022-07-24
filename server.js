076f30c74023e2d6f3ff2662e08e8d931ca9d8b6
Ivan Semikov <semikov.exf@gmail.com>
2022-07-24 20:00:58 +0300

    updates to deploy to heroku x2

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/hearthstone-card-game-angular'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/hearthstone-card-game-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
